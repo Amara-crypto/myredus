85ab9d24db6cd60834c7ffa9510f830b5a9897f9
PC1 <thomastarawali@gmail.com>
2020-09-07 12:48:02 +0000

    Added the removeProduct type

--- src/actions/addAction.js
+++ src/actions/addAction.js
@@ -1,12 +1,19 @@
-import {ADD_PRODUCT_BASKET} from './types';
+import { ADD_PRODUCT_BASKET, REMOVE_PRODUCT } from "./types";
 
-export const addBasket = (productName ) => {
-    return (dispatch) => {
-        console.log("add to Basket");
-        console.log("product added: ", productName)
-        dispatch({
-            type:ADD_PRODUCT_BASKET,
-            payload: productName
-        });
-    }
-}
+export const addBasket = (productName) => {
+	return (dispatch) => {
+		console.log("add to Basket");
+		console.log("product added: ", productName);
+		dispatch({
+			type: ADD_PRODUCT_BASKET,
+			payload: productName,
+		});
+	};
+};
+
+export const removeProduct = (productName) => {
+	return {
+		type: REMOVE_PRODUCT,
+		payload: productName,
+	};
+};
