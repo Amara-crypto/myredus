be059caa83bb9049ce42f461cd8afc6328839d23
PC1 <thomastarawali@gmail.com>
2020-09-07 12:45:45 +0000

    Added new handler for remove

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,82 +1,72 @@
-import React, {Fragment} from 'react';
-import {connect} from 'react-redux';
-import { Link } from 'react-router-dom'
- class Cart extends React.Component {
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { removeProduct } from "../actions/addAction";
+class Cart extends React.Component {
+	//to remove the item completely
+	handleRemove = (item) => {
+		this.props.removeProduct({
+			id: item.id,
+		});
+	};
+	//to add the quantity
+	handleAddQuantity = (name) => {
+		this.props.addQuantity(name);
+	};
+	//to substruct from the quantity
+	handleSubtractQuantity = (name) => {
+		this.props.subtractQuantity(name);
+	};
+	render() {
+		let addedItems = this.props.items.length;
+		let items = this.props.items;
+		console.log(addedItems + "UANITY");
+		console.log(items);
+		return addedItems > 0 ? (
+			<div className='container'>
+				{items.map((item) => (
+					<li className='collection-item avatar' key={item.name}>
+						<div className='item-img'>
+							<img src={item.image} alt={item.desc} className='' />
+						</div>
 
-    //to remove the item completely
-    handleRemove = (name)=>{
-        this.props.removeItem(name);
-    }
-    //to add the quantity
-    handleAddQuantity = (name)=>{
-        this.props.addQuantity(name);
-    }
-    //to substruct from the quantity
-    handleSubtractQuantity = (name)=>{
-        this.props.subtractQuantity(name);
-    }
-    render(){
-        let addedItems = this.props.items.length;
-        let items = this.props.items
-        console.log(addedItems+"UANITY")
-        console.log(items)
-        return(
-            (addedItems>0?
-            <div className="container">
-                {items.map((item) =>(
-                    <li className="collection-item avatar" key={item.name}>
-                    <div className="item-img"> 
-                        <img src={item.image} alt={item.desc} className=""/>
-                    </div>
-                
-                    <div className="item-desc">
-                        <span className="title">{item.desc}</span>
-                        <p>{item.desc}</p>
-                        <p><b>Price: {item.price}$</b></p> 
-                        <p>
-                            <b>Quantity: {item.quantity}</b> 
-                        </p>
-                        <Link to="/cart"><i className="material-icons">arrow_drop_up</i></Link>
-                        <Link to="/cart"><i className="material-icons">arrow_drop_down</i></Link>
-                        <button className="Remove">Remove</button>
-                    </div>
-                    
-                </li>
-                ))}
-            </div>:
-            <p>Nothing.</p>
-        ))
-    }
+						<div className='item-desc'>
+							<span className='title'>{item.desc}</span>
+							<p>{item.desc}</p>
+							<p>
+								<b>Price: {item.price}$</b>
+							</p>
+							<p>
+								<b>Quantity: {item.quantity}</b>
+							</p>
+							<Link to='/cart'>
+								<i className='material-icons'>arrow_drop_up</i>
+							</Link>
+							<Link to='/cart'>
+								<i className='material-icons'>arrow_drop_down</i>
+							</Link>
+							<button onclick={this.handleRemove(item)} className='Remove'>
+								Remove
+							</button>
+						</div>
+					</li>
+				))}
+			</div>
+		) : (
+			<p>Nothing.</p>
+		);
+	}
 }
 
+const mapStateToProps = (state) => {
+	return {
+		items: state.basketState.products,
+	};
+};
+const mapDispatchToProps = (dispatch) => {
+	return {
+		removeProduct: (item) => dispatch(removeProduct(item)),
+	};
+};
 
-
-       /* <Fragment>
-            <div>
-           <ul>
-           list={this.props.basketItems}
-           </ul>
-                
-            </div>
-        </Fragment>
-        
-    )
-}*/
-    const mapStateToProps = (state) => {
-        return{
-            items: state.basketState.products
-           // items: state.addedItems
-        }
-        //basketProps : state.basketState.products
-    }
-   /* const mapDispatchToProps(dispatch) =>{
-        return{
-            ...
-        }
-    } */
-
-    export default connect (mapStateToProps) (Cart);
-
-
-
-
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
